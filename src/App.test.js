22da0b717ab8358c7f91b6af423b7cec145cc870
rakshitranpariya <rakshitranpariya@gmail.com>
2026-01-16 02:07:37 -0400

    Importing the ThemeProvider in the App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-// test('renders the portfolio heading', () => {
-//   render(<App />);
-//   expect(screen.getByRole('heading', { name: /portfolio/i })).toBeInTheDocument();
-// });
+import { ThemeProvider } from "./contexts/ThemeContext";
+
 test("renders App without crashing", () => {
   render(
     <ThemeProvider>
